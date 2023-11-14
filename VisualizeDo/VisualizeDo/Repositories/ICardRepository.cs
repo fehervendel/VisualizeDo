3ad66a26bf87977beba8ab58addba0fe74c5d299
fehervendel <fehervendel@gmail.com>
2023-11-14 12:47:40 +0100

    feature: add ICardRepository.cs interface and methods

---
+++ VisualizeDo/VisualizeDo/Repositories/ICardRepository.cs
@@ -0,0 +1,12 @@
+﻿using VisualizeDo.Models;
+
+namespace VisualizeDo.Repositories;
+
+public interface ICardRepository
+{
+    Task<IEnumerable<Card>> GetAll();
+    Task<Card> GetById(int id);
+    Task Add(Card card);
+    Task Update(Card card);
+    Task DeleteById(int id);
+}
