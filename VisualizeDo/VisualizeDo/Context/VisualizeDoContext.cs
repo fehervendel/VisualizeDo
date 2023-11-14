d4b15a37a8e3caab9bc011bdde6b7792c596df5b
fehervendel <fehervendel@gmail.com>
2023-11-14 11:07:35 +0100

    feature: add VisualizeDo Context

---
+++ VisualizeDo/VisualizeDo/Context/VisualizeDoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using VisualizeDo.Models;
+
+namespace VisualizeDo.Context;
+
+public class VisualizeDoContext : DbContext
+{
+    public DbSet<Card> Cards { get; set; }
+    
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        DotNetEnv.Env.Load();
+        string connectionString = DotNetEnv.Env.GetString("CONNECTION_STRING");
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+}
