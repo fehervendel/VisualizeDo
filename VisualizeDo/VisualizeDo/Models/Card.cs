064cd65e5c5e4043acaad0da71824c83fd779d26
fehervendel <fehervendel@gmail.com>
2023-11-14 10:42:37 +0100

    feature: add Card model

---
+++ VisualizeDo/VisualizeDo/Models/Card.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VisualizeDo.Models;
+
+public class Card
+{
+    [Key]
+    public int Id { get; init; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? Priority { get; set; }
+    public string? Size { get; set; }
+}
