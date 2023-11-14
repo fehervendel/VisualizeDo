6b880bd8387b54881e6825892c525618fb03e7ad
fehervendel <fehervendel@gmail.com>
2023-11-14 12:48:04 +0100

    feature: implement ICardRepository methods

---
+++ VisualizeDo/VisualizeDo/Repositories/CardRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VisualizeDo.Context;
+using VisualizeDo.Models;
+
+namespace VisualizeDo.Repositories;
+
+public class CardRepository : ICardRepository
+{
+    public async Task<IEnumerable<Card>> GetAll()
+    {
+        using var dbContext = new VisualizeDoContext();
+        return await dbContext.Cards.ToListAsync();
+    }
+
+    public async Task<Card?> GetById(int id)
+    {
+        using var dbContext = new VisualizeDoContext();
+        return await dbContext.Cards.FirstOrDefaultAsync(c => c.Id == id);
+    }
+
+    public async  Task Add(Card card)
+    {
+        using var dbContext = new VisualizeDoContext();
+        dbContext.Add(card);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task Update(Card card)
+    {
+        using var dbContext = new VisualizeDoContext();
+        dbContext.Update(card);
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteById(int id)
+    {
+        using var dbContext = new VisualizeDoContext();
+        Card? cardToDelete = await dbContext.Cards.FirstOrDefaultAsync(c => c.Id == id);
+        if (cardToDelete != null)
+        {
+            dbContext.Remove(cardToDelete);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
