235cb510bc5d53d12a5ae65abad8d1a6191eb941
fehervendel <fehervendel@gmail.com>
2023-11-15 13:00:12 +0100

    feature: add Authentication to program.cs

--- VisualizeDo/VisualizeDo/Program.cs
+++ VisualizeDo/VisualizeDo/Program.cs
@@ -1,12 +1,22 @@
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using VisualizeDo.Context;
 using VisualizeDo.Models;
 using VisualizeDo.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var configBuilder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .AddUserSecrets<Program>();
+IConfiguration configuration = configBuilder.Build();
+var validIssuer = configuration["JwtSettings:ValidIssuer"];
+var validAudience = configuration["JwtSettings:ValidAudience"];
+var issuerSigningKey = configuration["JwtSettings:IssuerSigningKey"];
 // Add services to the container.
 AddServices();
-
+AddAuthentication();
 
 var app = builder.Build();
 
@@ -39,6 +49,34 @@ void InitializeDb()
    } 
 }
 //InitializeDb();
+
+void AddAuthentication()
+{
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                //ValidIssuer = jwtSettings["ValidIssuer"],
+                //ValidAudience = jwtSettings["ValidAudience"],
+                //IssuerSigningKey = new SymmetricSecurityKey(
+                //Encoding.UTF8.GetBytes(jwtSettings["IssuerSigningKey"])
+                ValidIssuer = validIssuer,
+                ValidAudience = validAudience,
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(issuerSigningKey)
+                ),
+            };
+        });
+}
+
 void AddServices()
 {
     builder.Services.AddControllers();
