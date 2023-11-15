a244e840f81efb2079ca9ec4f43a3495255cdfa7
fehervendel <fehervendel@gmail.com>
2023-11-15 13:54:34 +0100

    feature: add AuthRequest.cs, add AuthResponse.cs

---
+++ VisualizeDo/VisualizeDo/Models/DTOs/AuthRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace VisualizeDo.Models.DTOs;
+
+public record AuthRequest(string Email, string Password);
---
+++ VisualizeDo/VisualizeDo/Models/DTOs/AuthResponse.cs
@@ -0,0 +1,3 @@
+﻿namespace VisualizeDo.Models.DTOs;
+
+public record AuthResponse(string Email, string UserName, string Token);
