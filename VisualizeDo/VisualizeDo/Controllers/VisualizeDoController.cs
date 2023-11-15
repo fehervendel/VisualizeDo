a65e0cc2e44638e2e02cc5bf4bc28bd817d02309
fehervendel <fehervendel@gmail.com>
2023-11-15 14:04:12 +0100

    feature: add authorization to GetAllCards endpoint

--- VisualizeDo/VisualizeDo/Controllers/VisualizeDoController.cs
+++ VisualizeDo/VisualizeDo/Controllers/VisualizeDoController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using VisualizeDo.Context;
 using VisualizeDo.Models;
@@ -18,7 +19,7 @@ public class VisualizeDoController : ControllerBase
         _cardRepository = cardRepository;
     }
 
-    [HttpGet("GetAllCards")]
+    [HttpGet("GetAllCards"), Authorize(Roles = "User, Admin")]
     public async Task<ActionResult<Card>> GetAllCards()
     {
         var cards = await _cardRepository.GetAll();
