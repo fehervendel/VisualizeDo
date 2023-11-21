0f67e2cbb96f41122aaee8efde3750879e9ba353
fehervendel <fehervendel@gmail.com>
2023-11-21 15:23:53 +0100

    feature: add list model

---
+++ VisualizeDo/VisualizeDo/Models/List.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace VisualizeDo.Models;
+
+public class List
+{
+    [Key]
+    public int Id { get; init; }
+    public string? Name { get; set; }
+    public List<Card>? Cards { get; set; }
+    public int BoardId { get; set; }
+    [JsonIgnore]
+    public Board Board { get; set; }
+}
