2a2820193299f9c8e01a61944595f18e3d47f080
fehervendel <fehervendel@gmail.com>
2023-11-21 15:23:44 +0100

    feature: add board model

---
+++ VisualizeDo/VisualizeDo/Models/Board.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+
+namespace VisualizeDo.Models;
+
+public class Board
+{
+    [Key]
+    public int Id { get; init; }
+    public string? Name { get; set; }
+    public List<List>? Lists { get; set; }
+    public int UserId { get; set; }
+    [JsonIgnore]
+    public User User { get; set; }
+}
