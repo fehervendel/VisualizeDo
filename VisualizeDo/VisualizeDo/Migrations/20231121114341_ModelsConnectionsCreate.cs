c350888c8e849c7490be32c867c937ce8e61b6c5
fehervendel <fehervendel@gmail.com>
2023-11-21 15:23:13 +0100

    feature: add ModelsConnections migration

---
+++ VisualizeDo/VisualizeDo/Migrations/20231121114341_ModelsConnectionsCreate.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VisualizeDo.Migrations
+{
+    public partial class ModelsConnectionsCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ListId",
+                table: "Cards",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "Boards",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Boards", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Boards_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BoardId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lists", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Lists_Boards_BoardId",
+                        column: x => x.BoardId,
+                        principalTable: "Boards",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cards_ListId",
+                table: "Cards",
+                column: "ListId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Boards_UserId",
+                table: "Boards",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lists_BoardId",
+                table: "Lists",
+                column: "BoardId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cards_Lists_ListId",
+                table: "Cards",
+                column: "ListId",
+                principalTable: "Lists",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cards_Lists_ListId",
+                table: "Cards");
+
+            migrationBuilder.DropTable(
+                name: "Lists");
+
+            migrationBuilder.DropTable(
+                name: "Boards");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Cards_ListId",
+                table: "Cards");
+
+            migrationBuilder.DropColumn(
+                name: "ListId",
+                table: "Cards");
+        }
+    }
+}
