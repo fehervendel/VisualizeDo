e430b26b22afb14e2e78bfe4d70e7e1aaa4c38bd
fehervendel <fehervendel@gmail.com>
2023-11-21 15:26:18 +0100

    feature: add IListRepository.cs, implement ListRepository.cs

---
+++ VisualizeDo/VisualizeDo/Repositories/IListRepository.cs
@@ -0,0 +1,12 @@
+﻿using VisualizeDo.Models;
+
+namespace VisualizeDo.Repositories;
+
+public interface IListRepository
+{
+    Task<IEnumerable<List>> GetAll();
+    Task<List> GetById(int id);
+    Task Add(List list);
+    Task Update(List list);
+    Task DeleteById(int id);
+}
---
+++ VisualizeDo/VisualizeDo/Repositories/ListRepository.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VisualizeDo.Context;
+using VisualizeDo.Models;
+
+namespace VisualizeDo.Repositories;
+
+public class ListRepository : IListRepository
+{
+    public async Task<IEnumerable<List>> GetAll()
+    {
+        using var dbContext = new VisualizeDoContext();
+        return await dbContext.Lists.Include(l => l.Cards).ToListAsync();
+    }
+
+    public async Task<List> GetById(int id)
+    {
+        using var dbContext = new VisualizeDoContext();
+        return await dbContext.Lists.Include(l => l.Cards).FirstOrDefaultAsync(l => l.Id == id);
+    }
+
+    public async Task Add(List list)
+    {
+        using var dbContext = new VisualizeDoContext();
+
+        var board = await dbContext.Boards.FindAsync(list.BoardId);
+        if (board != null)
+        {
+            list.Board = board;
+            dbContext.Lists.Add(list);
+            await dbContext.SaveChangesAsync();
+        }
+        else
+        {
+            throw new ArgumentException("Board not found for the provided Board Id");
+        }
+    }
+
+    public async Task Update(List list)
+    {
+        using var dbContext = new VisualizeDoContext();
+        dbContext.Entry(list).State = EntityState.Modified;
+        await dbContext.SaveChangesAsync();
+    }
+
+    public async Task DeleteById(int id)
+    {
+        using var dbContext = new VisualizeDoContext();
+        var list = await dbContext.Lists.FindAsync(id);
+        if (list != null)
+        {
+            dbContext.Lists.Remove(list);
+            await dbContext.SaveChangesAsync();
+        }
+    }
+}
