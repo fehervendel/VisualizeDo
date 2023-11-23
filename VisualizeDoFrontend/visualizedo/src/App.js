3843188e175352c1b8ac7a58a5812723578d9b25
fehervendel <fehervendel@gmail.com>
2023-11-23 13:15:30 +0100

    feature: add conditional rendering to App.js

--- VisualizeDoFrontend/visualizedo/src/App.js
+++ VisualizeDoFrontend/visualizedo/src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
+import LoginMenu from './Pages/LoginMenu/LoginMenu';
+import Menu from './Pages/Menu';
 
 function App() {
+  const isLoggedIn = false;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isLoggedIn ? <Menu/> : <LoginMenu/>}
     </div>
   );
 }
