218f35eaf1f244236701b697f19cdc463912676a
fehervendel <fehervendel@gmail.com>
2023-11-29 16:11:14 +0100

    feature: add rendering logic and logout to Menu.js

--- VisualizeDoFrontend/visualizedo/src/Pages/Menu.js
+++ VisualizeDoFrontend/visualizedo/src/Pages/Menu.js
@@ -1,5 +1,23 @@
+import Cookies from "js-cookie";
+import LoginMenu from "./LoginMenu/LoginMenu";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Menu() {
-    return(<div>Menu</div>)
+    const [token, setToken] = useState(Cookies.get("userToken"));
+    const navigate = useNavigate();
+
+    const handleLogout = (e) => {
+        e.preventDefault();
+        Cookies.remove("userToken");
+        setToken(null);
+        navigate("/");
+    }
+    console.log(token);
+    return(<div>
+                <div>Menu</div>
+                <button onClick={(e) => handleLogout(e)}>Logout</button>
+        </div>)
 }
 
 export default Menu;
