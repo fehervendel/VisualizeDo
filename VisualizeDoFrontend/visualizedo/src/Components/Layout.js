0a3df6242c966c0effb6f114dd660ce998b440df
fehervendel <fehervendel@gmail.com>
2023-12-04 15:24:06 +0100

    feature: extend Layout.js

--- VisualizeDoFrontend/visualizedo/src/Components/Layout.js
+++ VisualizeDoFrontend/visualizedo/src/Components/Layout.js
@@ -1,11 +1,24 @@
 import { useLocation } from 'react-router-dom';
 import LoginMenu from '../Pages/LoginMenu/LoginMenu';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Cookies from 'js-cookie';
 
 const Layout = () => {
     const location = useLocation();
+    const [token, setToken] = useState(Cookies.get("userToken"));
+    const navigate = useNavigate();
+
+    const handleLogout = (e) => {
+        e.preventDefault();
+        Cookies.remove("userToken");
+        setToken(null);
+        navigate("/");
+    }
 
     return(<div>{location.pathname === '/' ? (null) : (<div>
         Menu bar!
+        <button onClick={(e) => handleLogout(e)}>Logout</button>
     </div>)}</div>);
 }
 
