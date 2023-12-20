1c13f62f66287c570057a5fdc9bc470995a394f2
fehervendel <fehervendel@gmail.com>
2023-12-20 14:48:30 +0100

    feature: add error handling on empty input fields, add states, add conditional rendering error message

--- VisualizeDoFrontend/visualizedo/src/Components/AddModal.js
+++ VisualizeDoFrontend/visualizedo/src/Components/AddModal.js
@@ -1,22 +1,20 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import '../Pages/Menu/Menu.css';
 import API_URL from "../Pages/config";
 
 
 function AddModal(props) {
     const priorities = ["Urgent", "High", "Medium", "Low"];
-    const sizes = ["Tiny", "Small", "Medium", "Large", "X-Large"];
+    const sizes = ["X-Large", "Large", "Medium", "Small", "Tiny"];
     const [title, setTitle] = useState("");
     const [description, setDescription] = useState("");
     const [priority, setPriority] = useState("");
     const [size, setSize] = useState("");
+    const [warnings, setWarnings] = useState([]);
+    const [isError, setIsError] = useState(false);
 
     const addCard = async () => {
-        console.log(description);
-        console.log(title);
-        console.log(size);
-        console.log(priority);
         try {
             const response = await fetch(`${API_URL}/VisualizeDo/AddCard`, {
 
@@ -43,31 +41,67 @@ function AddModal(props) {
         }
     };
 
+    const handleSave = () => {
+        let isAnyError = false;
+        let warnings = [];
+        if(title === ""){
+            warnings.push("title")
+            isAnyError = true;
+            setIsError(true);
+        }
+        if(description === ""){
+            warnings.push("description")
+            isAnyError = true;
+            setIsError(true);
+        }
+        if(priority === ""){
+            warnings.push("priority")
+            isAnyError = true;
+            setIsError(true);
+        }
+        if(size === ""){
+            warnings.push("size")
+            isAnyError = true;
+            setIsError(true);
+        }
+        setWarnings(warnings);
+        if(!isAnyError){
+            addCard();
+        }
+    };
+
     return (<div className="modal-overlay">
         <div className="modal">
             <div className="modal-content">
                 <h2>Add Card</h2>
                 <h3>Title</h3>
-                <input onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Card Title" />
+                <input className='title-input' onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Add your card title..." />
                 <h3>Description</h3>
-                <input onChange={(e) => setDescription(e.target.value)} type="text" placeholder="Description" />
+                <textarea className='description-input' onChange={(e) => setDescription(e.target.value)} type="text" placeholder="Add your description here..."/>
+                <div className="dropdown-inputs">
+                <div>
                 <h3>Priority</h3>
-                <select onChange={(e) => setPriority(e.target.value)}>
+                <select className='priority-input' onChange={(e) => setPriority(e.target.value)}>
                     <option disabled selected>Choose priority...</option>
                     {priorities.map((p) => (
                         <option key={p} value={p}>{p}</option>
                     ))}
                 </select>
+                </div>
+                <div>
                 <h3>Size</h3>
-                <select onChange={(e) => setSize(e.target.value)}>
+                <select className="size-input" onChange={(e) => setSize(e.target.value)}>
                     <option disabled selected>Choose size...</option>
                     {sizes.map((s) => (
                         <option key={s} value={s}>{s}</option>
                     ))}
                 </select>
+                </div>
+                </div>
                 <button className="close-button" onClick={props.toggleAddModal}>Close</button>
             </div>
-            <button onClick={addCard} className="save-button">Save</button>
+            <button onClick={handleSave} className="save-button">Save</button>
+            {isError ? (<p className="save-warning">Please complete the following fields: {warnings.join(', ')}.</p>) : (null)}
         </div>
     </div>)
 }
