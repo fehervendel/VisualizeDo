9404001623671cbda0dff75197c20b02cda57754
fehervendel <fehervendel@gmail.com>
2023-12-29 15:16:23 +0100

    refactor: remove unnecessary useEffect

--- VisualizeDoFrontend/visualizedo/src/Components/AddModal.js
+++ VisualizeDoFrontend/visualizedo/src/Components/AddModal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import '../Pages/Menu/Menu.css';
 import API_URL from "../Pages/config";
 
