6f943159647128790b13172eb18a6ac7b6affcc5
fehervendel <fehervendel@gmail.com>
2023-12-29 15:17:27 +0100

    feature: add editing feature, fetch, and elements to EditModal.js

--- VisualizeDoFrontend/visualizedo/src/Components/EditModal.js
+++ VisualizeDoFrontend/visualizedo/src/Components/EditModal.js
@@ -4,8 +4,42 @@ import API_URL from "../Pages/config";
 import '../Pages/Menu/Menu.css';
 
 function EditModal(props) {
+    const priorities = ["Urgent", "High", "Medium", "Low"];
+    const sizes = ["X-Large", "Large", "Medium", "Small", "Tiny"];
     const [confirmationModal, setConfirmationModal] = useState(false);
+    const [title, setTitle] = useState(props.card.title);
+    const [description, setDescription] = useState(props.card.description);
+    const [priority, setPriority] = useState(props.card.priority);
+    const [size, setSize] = useState(props.card.size);
     
+    const handleSave = async () => {
+      try {
+          const response = await fetch(`${API_URL}/VisualizeDo/EditCard`, {
+
+              method: "PUT",
+              headers: {
+                  "Content-Type": "application/json",
+                  //"Authorization": `Bearer ${token}`
+              },
+              body: JSON.stringify({
+                  id: props.card.id,
+                  title: title,
+                  description: description,
+                  priority: priority,
+                  size: size
+              }),
+          })
+          if (!response.ok) {
+            throw new Error('Error deleting card');
+          } else {
+            props.fetchListByBoardId(props.boardId);
+            toggleConfirmationModal();
+            props.toggleEditModal();
+          }
+      } catch (error) {
+          console.error("Error:", error);
+      }
+  };
 
     const deleteCardById = async () => {
         try {
@@ -32,28 +66,34 @@ function EditModal(props) {
     <div className="modal">
         <div className="modal-content">
             <h2>Edit your card here!</h2>
-           {/*  <h3>Title</h3>
-            <input onChange={(e) => setTitle(e.target.value)} type="text" placeholder="Card Title" />
+            <h3>Title</h3>
+            <input className="title-input" onChange={(e) => setTitle(e.target.value)} type="text" value={title}/>
             <h3>Description</h3>
-            <input onChange={(e) => setDescription(e.target.value)} type="text" placeholder="Description" />
-            <h3>Priority</h3>
-            <select onChange={(e) => setPriority(e.target.value)}>
-                <option disabled selected>Choose priority...</option>
-                {priorities.map((p) => (
-                    <option key={p} value={p}>{p}</option>
-                ))}
-            </select>
-            <h3>Size</h3>
-            <select onChange={(e) => setSize(e.target.value)}>
-                <option disabled selected>Choose size...</option>
-                {sizes.map((s) => (
-                    <option key={s} value={s}>{s}</option>
-                ))}
-            </select>*/}
+            <textarea className='description-input' onChange={(e) => setDescription(e.target.value)} type="text" value={description}/>
+            <div className="dropdown-inputs">
+                <div>
+                <h3>Priority</h3>
+                <select className='priority-input' value={priority} onChange={(e) => setPriority(e.target.value)}>
+                    <option disabled selected>Choose priority...</option>
+                    {priorities.map((p) => (
+                        <option key={p} value={p}>{p}</option>
+                    ))}
+                </select>
+                </div>
+                <div>
+                <h3>Size</h3>
+                <select className="size-input" value={size} onChange={(e) => setSize(e.target.value)}>
+                    <option disabled selected>Choose size...</option>
+                    {sizes.map((s) => (
+                        <option key={s} value={s}>{s}</option>
+                    ))}
+                </select>
+                </div>
+                </div>
             <button className="close-button" onClick={props.toggleEditModal}>Close</button> 
         </div>
         <button className="delete-button" onClick={toggleConfirmationModal}>Delete card</button>
-        <button onClick className="save-button">Save</button>
+        <button className="save-button" onClick={handleSave}>Save</button>
     </div>
     {confirmationModal && (<div className="confirmation-modal-overlay">
     <div className="confirmation-modal">
