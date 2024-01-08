92691bc8e7a3e68250f23f0744cb8ce01d837070
fehervendel <fehervendel@gmail.com>
2024-01-08 14:45:45 +0100

    feature: add Create path

--- VisualizeDoFrontend/visualizedo/src/App.js
+++ VisualizeDoFrontend/visualizedo/src/App.js
@@ -3,8 +3,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import LoginMenu from './Pages/LoginMenu/LoginMenu';
-import Menu from './Pages/Menu/Menu';
+import Menu from './Pages/Boards/Boards';
 import Layout from './Components/Layout';
+import Create from './Pages/Create/Create';
 
 function App() {
 return (
@@ -13,6 +14,7 @@ return (
     <Routes>
       <Route path="/" element={<LoginMenu/>}></Route>
       <Route path="/Menu" element={<Menu/>}></Route>
+      <Route path="/Create" element={<Create/>}></Route>
     </Routes>
   </BrowserRouter>
 )
