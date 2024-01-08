b28eded97987ff7d189c7daae5c3b93cf346abf2
fehervendel <fehervendel@gmail.com>
2024-01-08 14:46:02 +0100

    feature: add navigation to Layout.js

--- VisualizeDoFrontend/visualizedo/src/Components/Layout.js
+++ VisualizeDoFrontend/visualizedo/src/Components/Layout.js
@@ -1,4 +1,4 @@
-import { useLocation } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
@@ -6,15 +6,26 @@ import './Layout.css';
 import useLogout from '../Hooks/useLogout';
 
 const Layout = () => {
+    const navigate = useNavigate();
    const {location,
     handleLogout} = useLogout();
 
+    const handleCreateClick = () => {
+        navigate('/create');
+    }
+
+    const handleBoardsClick = () => {
+        navigate('/Menu');
+    }
+
+
+
     return (<div>{location.pathname === '/' ? (null) : (<div className='menuBar'>
         <div className='menuItems'>
-            <a className='button'>Create</a>
+            <a className='button' onClick={handleCreateClick}>Create</a>
         </div>
         <div className='menuItems'>
-            <a className='button' id='boardButton'>Boards</a>
+            <a className='button' id='boardButton' onClick={handleBoardsClick}>Boards</a>
         </div>
         <div className='menuItems' id='logoutButton'>
             <a className='logoutButton' onClick={(e) => handleLogout(e)}>Logout</a>
