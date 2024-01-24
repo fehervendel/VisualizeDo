ce74d80b6cc25a7c8d3d985706ab558805db0338
fehervendel <fehervendel@gmail.com>
2024-01-24 13:09:05 +0100

    feature: add Card.service.js

---
+++ VisualizeDoFrontend/visualizedo/src/Services/Card.service.js
@@ -0,0 +1,16 @@
+import API_URL from "../Pages/config";
+
+export const changeCardList = async (cardId, listId) => {
+    try {
+        const response = await fetch(`${API_URL}/VisualizeDo/ChangeCardsListById?cardId=${cardId}&listId=${listId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            const data = await response.json();
+            return {status: response.status, data: data}
+    } catch (err) {
+        return { status: 500, data: err }
+    }
+};
