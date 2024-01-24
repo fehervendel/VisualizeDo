3912fe70fcb7c28efe97833bd5bd2b300ee65e23
fehervendel <fehervendel@gmail.com>
2024-01-24 14:07:19 +0100

    feature: add addNewBoard request

--- VisualizeDoFrontend/visualizedo/src/Services/Board.service.js
+++ VisualizeDoFrontend/visualizedo/src/Services/Board.service.js
@@ -1,12 +1,16 @@
 import API_URL from "../config";
+import Cookies from "js-cookie";
 
 export const boardNameChange = async (boardId, newBoardName) => {
     try {
         const response = await fetch(`${API_URL}/VisualizeDo/ChangeBoardName?id=${boardId}&newName=${newBoardName}`, {
-            method: "PUT"
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                //"Authorization": `Bearer ${token}`
+            },
         });
         const data = await response.json();
-        
         return {status: response.status, data: data}
     } catch (e) {
         return { status: 500, data: e}
@@ -16,12 +20,32 @@ export const boardNameChange = async (boardId, newBoardName) => {
 export const boardDelete = async (selectedBoardId) => {
     try {
         const response = await fetch(`${API_URL}/VisualizeDo/DeleteBoardById?id=${selectedBoardId}`, {
-            method: "DELETE"
+            method: "DELETE",
+            headers: {
+                "Content-Type": "text/plain",
+                //"Authorization": `Bearer ${token}`
+            },
         });
         const data = await response.text();
-        
         return {status: response.status, data: data}
-    } catch (e) {
-        return { status: 500, data: e}
+    } catch (err) {
+        return { status: 500, data: err}
+    }
+};
+
+export const addNewBoard = async (boardName) => {
+    try {
+        const userEmail = Cookies.get("userEmail");
+        const response = await fetch(`${API_URL}/VisualizeDo/AddBoard?email=${userEmail}&name=${boardName}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                //"Authorization": `Bearer ${token}`
+            },
+        })
+        const data = await response.json();
+        return {status: response.status, data: data}
+    } catch (err) {
+        return { status: 500, data: err}
     }
 };
