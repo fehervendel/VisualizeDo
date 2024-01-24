c029b2682749bcd090859459e8757d17cc49a04c
fehervendel <fehervendel@gmail.com>
2024-01-24 13:09:17 +0100

    feature: add List.service.js

---
+++ VisualizeDoFrontend/visualizedo/src/Services/List.service.js
@@ -0,0 +1,60 @@
+import API_URL from "../Pages/config";
+
+export const listNameChange = async (listNameEditId, newNameOfList) => {
+    try {
+        const response = await fetch(`${API_URL}/VisualizeDo/ChangeListName?listId=${listNameEditId}&newName=${newNameOfList}`, {
+            method: "PUT"
+        });
+        const data = await response.text();
+        
+        return {status: response.status, data: data}
+    } catch (e) {
+        return { status: 500, data: e}
+    }
+};
+
+export const addNewList = async (boardId, newListName) => {
+    try {
+      
+        const response = await fetch(`${API_URL}/VisualizeDo/AddList?boardId=${boardId}&name=${newListName}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                //"Authorization": `Bearer ${token}`
+            },
+        })
+        const data = await response.json();
+        return {status: response.status, data: data}
+    } catch (err) {
+        return { status: 500, data: err}
+    }
+}
+
+export const deleteList = async (deleteListId) => {
+    try {
+        const response = await fetch(`${API_URL}/VisualizeDo/DeleteListById?id=${deleteListId}`, {
+            method: "DELETE"
+        });
+        const data = await response.text();
+        return {status: response.status, data: data}
+    } catch (err) {
+        return { status: 500, data: err}
+    }
+}
+
+export const getListByBoardId = async (id) => {
+    try {
+        const response = await fetch(`${API_URL}/VisualizeDo/GetListsByBoardId?id=${id}`, {
+            method: "GET",
+            headers: {
+                //Authorization: `Bearer ${token}`
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+        });
+            const data = await response.json();
+            return {status: response.status, data: data}
+    } catch (err) {
+        return { status: 500, data: err}
+    }
+};
