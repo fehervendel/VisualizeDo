1954f8da9b44639c5dcc410227681dbc6e270664
fehervendel <fehervendel@gmail.com>
2024-01-24 13:40:14 +0100

    feature: add add, edit and delete requests

--- VisualizeDoFrontend/visualizedo/src/Services/Card.service.js
+++ VisualizeDoFrontend/visualizedo/src/Services/Card.service.js
@@ -14,3 +14,62 @@ export const changeCardList = async (cardId, listId) => {
         return { status: 500, data: err }
     }
 };
+
+export const add = async (listId, cardTitle, cardDescription, priority, size) => {
+    try {
+        const response = await fetch(`${API_URL}/VisualizeDo/AddCard`, {
+
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                //"Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                listId: listId,
+                title: cardTitle,
+                description: cardDescription,
+                priority: priority,
+                size: size
+            }),
+        })
+        const data = response.json();
+        return { status: response.status, data: data }
+    } catch (error) {
+        return { status: 500, data: error }
+    }
+};
+
+export const editCard = async (propsCardId, title, description, priority, size) => {
+    try {
+        const response = await fetch(`${API_URL}/VisualizeDo/EditCard`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                //"Authorization": `Bearer ${token}`
+            },
+            body: JSON.stringify({
+                id: propsCardId,
+                title: title,
+                description: description,
+                priority: priority,
+                size: size
+            }),
+        })
+        const data = await response.json();
+        return { status: response.status, data: data }
+    } catch (error) {
+        return { status: 500, data: error }
+    }
+};
+
+export const deleteCard = async (propsCardId) => {
+    try {
+      const response = await fetch(`${API_URL}/VisualizeDo/DeleteCardById?id=${propsCardId}`, {
+        method: 'DELETE',
+      });
+      const data = await response.json();
+      return { status: response.status, data: data }
+    } catch (error) {
+        return { status: 500, data: error }
+    }
+  };
