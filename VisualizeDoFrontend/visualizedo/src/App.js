e882df4194bd1cae08accdfc29f1a648f4145fef
fehervendel <fehervendel@gmail.com>
2024-01-24 12:21:51 +0100

    feature: add ToastContextProvider Route

--- VisualizeDoFrontend/visualizedo/src/App.js
+++ VisualizeDoFrontend/visualizedo/src/App.js
@@ -7,14 +7,18 @@ import Boards from './Pages/Boards/Boards';
 import Layout from './Components/Layout';
 import Create from './Pages/Create/Create';
 
+import { ToastContextProvider } from './Contexts/ToastContext.context';
+
 function App() {
 return (
   <BrowserRouter>
   <Layout/>
     <Routes>
-      <Route path="/" element={<LoginMenu/>}></Route>
-      <Route path="/Boards" element={<Boards/>}></Route>
-      <Route path="/Create" element={<Create/>}></Route>
+      <Route path='/' element={<ToastContextProvider/>}>
+        <Route path="/" element={<LoginMenu/>}></Route>
+        <Route path="/Boards" element={<Boards/>}></Route>
+        <Route path="/Create" element={<Create/>}></Route>
+      </Route>
     </Routes>
   </BrowserRouter>
 )
