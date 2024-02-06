8001b79475a2acd7ba0376c24ea5024a50f97dde
fehervendel <fehervendel@gmail.com>
2024-02-06 14:57:59 +0100

    feature: add AddBoard, AddList, AddCard initialization for setup, remove unnecessary code, fix bugs

--- VisualizeDo/VisualizeDoTest/VisualizeDoTest.cs
+++ VisualizeDo/VisualizeDoTest/VisualizeDoTest.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using System.Text.Json;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -13,6 +14,7 @@ using VisualizeDo.Models;
 using VisualizeDo.Models.DTOs;
 using VisualizeDo.Repositories;
 using VisualizeDo.Services.Authentication;
+using List = VisualizeDo.Models.List;
 using MediaTypeHeaderValue = System.Net.Http.Headers.MediaTypeHeaderValue;
 
 namespace VisualizeDoTest;
@@ -20,8 +22,11 @@ namespace VisualizeDoTest;
 public class VisualizeDoTest : WebApplicationFactory<Program>
 {
     private HttpClient _client;
-    private int _userId;
     private VisualizeDoContext _context;
+    private User? _user;
+    private Board? _board;
+    private List? _list;
+    private Card? _card;
 
     [SetUp]
     public async Task Setup()
@@ -29,25 +34,12 @@ public class VisualizeDoTest : WebApplicationFactory<Program>
         DotNetEnv.Env.Load();
         string connectionString = DotNetEnv.Env.GetString("CONNECTION_STRING");
         Environment.SetEnvironmentVariable("CONNECTION_STRING", "Server=localhost,1433;Database=VisualizeDo;User Id=sa;Password=Feher2023vendeL!;");
-        var options = new JsonSerializerOptions
-        {
-            PropertyNameCaseInsensitive = true
-        };
 
         _client = CreateClient();
-        AuthRequest authRequest = new AuthRequest("admin@admin.com", "admin123");
-        string jsonString = JsonSerializer.Serialize(authRequest);
-        StringContent jsonStringContent = new StringContent(jsonString);
-        jsonStringContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
-
-        var response = _client.PostAsync("Auth/Login", jsonStringContent).Result;
-        var content = response.Content.ReadAsStringAsync().Result;
-        var desContent = JsonSerializer.Deserialize<AuthResponse>(content, options);
-        var token = desContent.Token;
-
-        _client.DefaultRequestHeaders.Add("Authorization", $"Bearer {token}");
-        
         await InitializeTestUserByEmail();
+        await InitializeTestBoard();
+        await InitializeTestList();
+        await InitializeTestCard();
     }
     
     [OneTimeTearDown]
@@ -71,11 +63,11 @@ public class VisualizeDoTest : WebApplicationFactory<Program>
         { 
             await RegisterTestUser("testemail@testemail.com", "testuser", "testpassword");
             User newTestUser = await CheckTestUser();
-            _userId = newTestUser.Id;
+            _user = newTestUser;
         }
         else
         {
-            _userId = testUser.Id;
+            _user = testUser;
         }
     }
 
@@ -96,7 +88,7 @@ public class VisualizeDoTest : WebApplicationFactory<Program>
             {
                 PropertyNameCaseInsensitive = true
             };
-            User user = JsonSerializer.Deserialize<User>(content, options);
+            User? user = JsonSerializer.Deserialize<User>(content, options);
             return user;
         }
         catch (HttpRequestException ex)
@@ -114,6 +106,107 @@ public class VisualizeDoTest : WebApplicationFactory<Program>
         await _client.PostAsync("/Auth/Register", jsonStringContent);
     }
 
+    private async Task InitializeTestBoard()
+    {
+        if (_user.Boards == null || _user.Boards.Count == 0 || _user.Boards[0] == null)
+        {
+            _board = await CreateTestBoard();
+        }
+        else
+        {
+            _board = _user.Boards[0];
+        }
+    }
+
+    private async Task InitializeTestList()
+    {
+        if (_board.Lists == null || _board.Lists.Count == 0 || _board.Lists[0] == null)
+        {
+            _list = await CreateList();
+        }
+        else
+        {
+            _list = _board.Lists[0];
+        }
+    }
+
+    private async Task InitializeTestCard()
+    {
+        if (_list.Cards == null || _list.Cards.Count == 0 || _list.Cards[0] == null)
+        {
+            _card = await CreateCard();
+        }
+        else
+        {
+            _card = _list.Cards[0];
+        }
+    }
+    private async Task<Board?> CreateTestBoard()
+    {
+        string apiUrl = $"/VisualizeDo/AddBoard?email={_user?.EmailAddress}&name=VisualizeDoTestBoard";
+        var response = await _client.PostAsync(apiUrl, null);
+        response.EnsureSuccessStatusCode();
+
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+        var responseContent = await response.Content.ReadAsStringAsync(); 
+        Board? board = JsonSerializer.Deserialize<Board>(responseContent, options);
+
+        _board = board;
+        return board;
+    }
+
+    private async Task<List> CreateList()
+    {
+        string apiUrl = $"/VisualizeDo/AddList?boardId={_board.Id}&name=TestList";
+        var response = await _client.PostAsync(apiUrl, null);
+        response.EnsureSuccessStatusCode();
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var list = JsonSerializer.Deserialize<List>(responseContent, options);
+
+        _list = list;
+        return list;
+    }
+    
+    private async Task<Card?> CreateCard()
+    {
+        var cardData = new Dictionary<string, object>
+        {
+            { "listId", _list.Id },
+            { "title", "TestTitle" },
+            { "description", "TestDescription" },
+            { "priority", "Urgent" },
+            { "size", "Large" }
+        };
+        
+        var jsonData = JsonSerializer.Serialize(cardData);
+        var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+        
+        string apiUrl = $"/VisualizeDo/AddCard";
+        var response = await _client.PostAsync(apiUrl, content);
+        
+        response.EnsureSuccessStatusCode();
+        
+        var options = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var card = JsonSerializer.Deserialize<Card>(responseContent, options);
+
+        _card = card;
+
+        return card;
+    }
+    
     [Test]
     public void Test1()
     {
