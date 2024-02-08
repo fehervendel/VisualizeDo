09256b2e10841f3206d43a56e501ea4bd4e4ad8f
fehervendel <fehervendel@gmail.com>
2024-02-08 11:19:50 +0100

    feature: add GetUserByEmail tests, add Clean up method to it

--- VisualizeDo/VisualizeDoTest/VisualizeDoTest.cs
+++ VisualizeDo/VisualizeDoTest/VisualizeDoTest.cs
@@ -5,15 +5,17 @@ using List = VisualizeDo.Models.List;
 
 
 namespace VisualizeDoTest;
+
 [TestFixture]
 public class VisualizeDoTest : WebApplicationFactory<Program>
 {
     private HttpClient _client;
-    
+
     private User? _user;
     private Board? _board;
     private List? _list;
     private Card? _card;
+
     private JsonSerializerOptions _options = new JsonSerializerOptions
     {
         PropertyNameCaseInsensitive = true
@@ -26,8 +28,9 @@ public class VisualizeDoTest : WebApplicationFactory<Program>
     {
         DotNetEnv.Env.Load();
         string connectionString = DotNetEnv.Env.GetString("CONNECTION_STRING");
-        Environment.SetEnvironmentVariable("CONNECTION_STRING", "Server=localhost,1433;Database=VisualizeDo;User Id=sa;Password=Feher2023vendeL!;");
-        
+        Environment.SetEnvironmentVariable("CONNECTION_STRING",
+            "Server=localhost,1433;Database=VisualizeDo;User Id=sa;Password=Feher2023vendeL!;");
+
         _client = CreateClient();
         await _testPrepare.Prepare();
         _user = _testPrepare.User;
@@ -37,14 +40,14 @@ public class VisualizeDoTest : WebApplicationFactory<Program>
         // TestContext.Progress.WriteLine(_user.Name);
         // TestContext.Progress.WriteLine(_board.Name);
     }
-    
+
     [OneTimeTearDown]
     public void TearDown()
     {
         _client.Dispose();
     }
-    
-    
+
+
     [Test]
     public async Task GetUserByEmailReturnsOk()
     {
@@ -53,14 +56,56 @@ public class VisualizeDoTest : WebApplicationFactory<Program>
 
         response.EnsureSuccessStatusCode();
 
-        
+
         var content = await response.Content.ReadAsStringAsync();
         var user = JsonSerializer.Deserialize<User>(content, _options);
-        
+
         Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
         Assert.IsNotNull(user);
         Assert.That(user.Id, Is.EqualTo(_user.Id));
         //Assert.That(user.Boards[0].Lists[0].Cards[0], Is.DeepEqualTo(_user.Boards[0].Lists[0].Cards[0])); //HOW
         Assert.That(user.Name, Is.EqualTo(_user.Name));
+    }
+
+    [Test]
+    public async Task GetUserByEmailReturnsNotFound()
+    {
+        string email = "123@123.com";
+        var apiUrl = $"/VisualizeDo/GetUserByEmail?email={email}";
+        var response = await _client.GetAsync(apiUrl);
+
+        response.EnsureSuccessStatusCode();
+
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.NoContent));
+    }
+
+    [Test]
+    public async Task AddNewBoardShouldReturnOk()
+    {
+        string boardName = "AddBoardTest";
+        var apiUrl = $"/VisualizeDo/AddBoard?email={_user.EmailAddress}&name={boardName}";
+        var response = await _client.PostAsync(apiUrl, null);
+        response.EnsureSuccessStatusCode();
+
+        var userResponse = await _client.GetAsync("/VisualizeDo/GetUserByEmail?email=testemail@testemail.com");
+        var userContent = await userResponse.Content.ReadAsStringAsync();
+        User? user = JsonSerializer.Deserialize<User>(userContent, _options);
+        _user = user;
+
+        var content = await response.Content.ReadAsStringAsync();
+        var board = JsonSerializer.Deserialize<Board>(content, _options);
+
+        Assert.That(response.StatusCode, Is.EqualTo(System.Net.HttpStatusCode.OK));
+        Assert.IsNotNull(board);
+        Assert.That(board.Name, Is.EqualTo(boardName));
+        Assert.IsTrue(_user.Boards.Any(b => b.Id == board.Id));
+
+        await AddNewBoardShouldReturnOk_CleanUp(board.Id);
+    }
+    private async Task AddNewBoardShouldReturnOk_CleanUp(int boardId)
+    {
+        var apiUrl = $"/VisualizeDo/DeleteBoardById?id={boardId}";
+        var response = await _client.DeleteAsync(apiUrl);
+        response.EnsureSuccessStatusCode();
     }
 }
